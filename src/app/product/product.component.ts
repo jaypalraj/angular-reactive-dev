7e15bd09c1f2c6d8293084edff17e24cc7f32bde
Jaypal Raj <jaypalraj@hotmail.com>
2020-02-03 20:43:29 +0000

    Added category on selection event handler

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../core/services/product.service';
 import { ProductCategoryService } from '../core/services/product-category.service';
+import { Subject, BehaviorSubject, combineLatest, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 
 @Component({
@@ -10,16 +12,35 @@ import { ProductCategoryService } from '../core/services/product-category.servic
 })
 export class ProductComponent implements OnInit {
 
+
+  productCategories$ = this.productCategoryService.categories$;
+
+
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();
+
   constructor(private productService:ProductService
             , private productCategoryService:ProductCategoryService) { }
 
-  products$ = this.productService.products$;
-  productCategories$ = this.productCategoryService.categories$;
-  productWithCategory$ = this.productService.productWithCategory$;
+  
+  products$ = combineLatest([
+    this.productService.productWithCategory$,
+    this.categorySelectedAction$
+  ]).pipe(
+      map(([products,selectedCategoryId]) => 
+        products.filter(product => selectedCategoryId ? product.categoryId === selectedCategoryId : true)  
+    ),catchError(err => {
+        console.log(err);
+        return EMPTY;
+    })
+  );
 
 
   ngOnInit() {
 
   }
 
+  OnSelectedCategoryChanged(categoryId: string): void{
+    this.categorySelectedSubject.next(+categoryId);
+  }
 }
